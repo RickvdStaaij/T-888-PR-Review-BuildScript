00558d455cbc6612a3b4fb5bfa61d1faaa4b98b0
Rick van der Staaij <info@futureportal.com>
2015-01-05 23:20:34 +0100

    Send raw file for T-888 to resolve

--- run.php
+++ run.php
@@ -4,18 +4,20 @@ require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
+$result = [
+    'build' => 'done'
+];
+
 $phpCsFile = 'dev/reports/checkstyle.xml';
 if (file_exists($phpCsFile)) {
     $phpCsXml = file_get_contents($phpCsFile);
-    $phpcs = json_encode((array) simplexml_load_string($phpCsXml));
-} else {
-    $phpcs = json_encode([]);
+    $result['phpcs'] = $phpCsXml;
 }
 
 $client = new Client();
 $response = $client->post('http://t888.futureportal.com/result-build.php', [
     'body' => [
-        'phpcs' => $phpcs,
+        $result
     ],
 ]);
 
