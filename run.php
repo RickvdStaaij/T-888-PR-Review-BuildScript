11e9587f59f8d587d6ebc3624cc1266040e89d1e
Rick van der Staaij <info@futureportal.com>
2015-01-05 22:29:09 +0100

    Add phpcs document report check

--- run.php
+++ run.php
@@ -4,10 +4,18 @@ require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
+$phpCsFile = 'dev/reports/checkstyle.xml';
+if (file_exists($phpCsFile)) {
+    $phpCsXml = require $phpCsFile;
+    $phpcs = json_encode((array) simplexml_load_string($phpCsXml));
+} else {
+    $phpcs = json_encode([]);
+}
+
 $client = new Client();
 $response = $client->post('http://t888.futureportal.com/result-build.php', [
     'body' => [
-        'test' => 'omg this is an actual test result',
+        'phpcs' => $phpcs,
     ],
 ]);
 
