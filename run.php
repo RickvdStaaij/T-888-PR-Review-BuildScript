54ba7812d49c740737b1ae4796dd93172e06c205
Rick van der Staaij <info@futureportal.com>
2015-01-05 20:05:23 +0100

    Let guzzle send a post body

--- run.php
+++ run.php
@@ -6,7 +6,9 @@ use GuzzleHttp\Client;
 
 $client = new Client();
 $response = $client->post('http://t888.futureportal.com/result-build.php', [
-    'test' => 'omg this is an actual test result'
+    'body' => [
+        'test' => 'omg this is an actual test result',
+    ],
 ]);
 
 echo $response->getBody();
